6d3f2500b66ed759944c9618bb6889f710b7530b
Ninio <pereriksson98@hotmail.com>
2020-02-01 15:11:22 +0100

    Repair station
    test

---
+++ Unity_OverRepaired/Assets/_Scripts/RepairStation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepairStation : MonoBehaviour
+{
+    [SerializeField] string type;
+    public float workTime;
+    private float timer;
+    public Transform placePosition;
+    public void work(RobotPart part)
+    {
+        if (part.getCurrentRepair() == type)
+        {
+            if (timer < workTime)
+            {
+                timer += Time.deltaTime;
+            }
+            else
+            {
+                part.currentRepairDone();
+            }
+        }
+    }
+    public void resetWork()
+    {
+        timer = 0;
+    }
+}
--- Unity_OverRepaired/Assets/_Scripts/RobotPart.cs
+++ Unity_OverRepaired/Assets/_Scripts/RobotPart.cs
@@ -6,6 +6,11 @@ using UnityEngine;
 public class RobotPart : MonoBehaviour
 {
     public bool isBroken = false;
+
+    [SerializeField]
+    private string[] repairOrder = new string[4];
+    private Stack<string> repairOrderStack = new Stack<string>();
+
     [Serializable]
     public enum parts
     {
@@ -18,5 +23,28 @@ public class RobotPart : MonoBehaviour
     }
     public parts whatPart;
 
+    public void Start()
+    {
+        for (int i = repairOrder.Length - 1; i >= 0; i--)
+        {
+            repairOrderStack.Push(repairOrder[i]);
+        }
+    }
 
+    public string getCurrentRepair()
+    {
+        return repairOrderStack.Peek();
+    }
+
+    public void currentRepairDone()
+    {
+        if (repairOrderStack.Count > 0)
+        {
+            if (repairOrderStack.Count == 1)
+            {
+                isBroken = false;
+            }
+            repairOrderStack.Pop();
+        }
+    }
 }
